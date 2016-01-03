34be05f4a8789258ffc3a9e620b382fce57d8887
cweioo <jfgh431819@126.com>
2016-01-03 20:37:41 +0800

    add HomeViewController

---
+++ Gogo/Gogo/Home/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Gogo
+//
+//  Created by ChenWei on 16/1/3.
+//  Copyright © 2016年 cw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
